7afd1044aa8d220bbf84fae569cac71664dd05f9
xBIGMAN <ally.jan@mail.ru>
2014-11-18 23:04:39 +0300

    test

---
+++ PWD/PWD/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // new comment
+            // new new comment
+            //try
+        }
+        public newMethod()
+        {
+
+        }
+    }
+}
